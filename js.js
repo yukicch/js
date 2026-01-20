f9171ab037cb2406af1c2b46ae806344011de9ba
yukicch <193929839+yukicch@users.noreply.github.com>
2026-01-21 00:24:21 +0300

    bg

---
+++ js.js
@@ -0,0 +1,16 @@
+const User = function(email , password){
+
+    this.email = email;
+    this.password = password;
+}
+User.function = function (kok){
+    console.log(kok)
+}
+const mango = new User('mak', 'lol')
+console.log(mango);
+User.function('kfkfkfask')
+
+
+console.log(mango);
+
+ 
